88a89b4fa6103fcbd086aa84c9c7cf840401c7ca
Bernhard Henritzi <191458278+bhenritzi@users.noreply.github.com>
2025-05-17 19:29:33 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import logo from './assets/logo.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <a href="https://henritzi.dev">
+          <img src={logo} className="logo" alt="Logo" />
+        </a>
+      </div>
+      <h2>Hi! I am Bernhard Henritzi. This website is currently under construction.</h2>
+    </>
+  )
+}
+
+export default App
